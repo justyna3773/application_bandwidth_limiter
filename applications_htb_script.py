f337ee6cf2b7f557a96323a67999256214025e76
jhnatiuk <justyna3773@gmail.com>
2021-04-22 14:03:39 +0200

    updated comments

--- applications_htb_script.py
+++ applications_htb_script.py
@@ -7,12 +7,12 @@ import logging
 
 from config_parser import load_config, app_dict_ip
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 7150        # The port used by the server
+HOST = '127.0.0.1'   # Nazwa lub adres IP serwera
+PORT = 7150         # Port dla netifyd
 
 
 
-def set_rule(options, src_ip):#przyjmuje liste ustawionych opcji odpowiadajaca aplikacji w slowniku
+def set_rule(options, src_ip):#przyjmuje liste ustawionych opcji odpowiadajaca aplikacji w slowniku oraz adres IP wykryty przez netifyd
     
     command = 'set_download '
     command += f'-d {options["dest_network"]} ' if 'dest_network' in options else ''
@@ -22,7 +22,7 @@ def set_rule(options, src_ip):#przyjmuje liste ustawionych opcji odpowiadajaca a
     command += f'-m {options["loss"]} ' if 'loss' in options else ''
     command += f'-p {options["port"]} ' if 'port' in options else ''   
     print(command) 
-    proc = subprocess.Popen(['bash','-c', 'source ./htb_script.sh;' + command],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(['bash','-c', 'source ./htb_script.sh;' + command],stdout=subprocess.PIPE, stderr=subprocess.PIPE)# wywolanie stworzonej komendy dla skryptu htb_script.sh
 
 
 
@@ -32,39 +32,23 @@ def remove_application_rule(interface, ip_addr, rate):
         
 
 
-def modify_rule(interface, addr, rate):
-    get_current_rules = subprocess.run(f'tcshow {interface}', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
 
-    current_rules_addresses = []
-    rules_dict = json.loads(get_current_rules.stdout)
-    if interface in rules_dict:
-        for key in rules_dict["br-lan"]["outgoing"]:
-            address = key[key.find('=')+1:key.find('/')]
-            current_rules_addresses.append(address)
-            if addr in current_rules_addresses:
-                modified_rule = subprocess.run(f'tcset {interface} --rate {rate}Kbps --src-network {address} --overwrite',stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
-            else:
-                new_rule = subprocess.run(f'tcset {interface} --rate {rate}Kbps --src-network {addr} --add',stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
-
-
-def setup(name, intercepted):
-    #interfaces = ['eth0', 'br-lan']
+def setup(name, intercepted):#funkcja setup przyjmuje nazwe pliku konfiguracyjnego i pliku z przechwyconymi adresami
     config_parser_dict = load_config(name)
-    addr_dict = app_dict_ip(config_parser_dict)
-    inter_dict = {}#miejsce na nowe adresy wg aktualnego pliku config
+    addr_dict = app_dict_ip(config_parser_dict)# stworzenie slownika na zaktualizowane (wszystkie) adresy IP
+    inter_dict = {}# slownik na adresy IP przechwycone do ip_intercepted
     
     subprocess.Popen(['bash', '-c', 'source ./htb_script.sh; remove_download; htb_init'])
-    
-    # usunieto poprzednie tc configi
-    if os.path.isfile(intercepted) and os.access(intercepted, os.R_OK):
-        with open(intercepted,'r') as jsonfile:
-            inter_dict = json.load(jsonfile)
+    # usuniecie wszystkich poprzednich zasad dla interfejsu
+    if os.path.isfile(intercepted) and os.access(intercepted, os.R_OK):#jesli plik intercepted istnieje
+        with open(intercepted,'r') as jsonfile:#otwarcie pliku
+            inter_dict = json.load(jsonfile)#zaladowanie poprzednich adresow do slownika
             inter_dict = {app: set(inter_dict[app]) for app in inter_dict}
             if inter_dict:   #jesli cos jest w tym slowniku to ustawiam te zasady ponownie     
                 for app,ips in inter_dict.items():#teraz dla kazdej aplikacji musze uwzglednic config obecny
-                    #teraz uwzgleniam pojedyncze ip'
+                    #teraz uwzgleniam pojedyncze ip dla aplikacji
                     for i in ips:
-                        set_rule(config_parser_dict[app], i)
+                        set_rule(config_parser_dict[app], i)#wywoluje funkcje skryptu htb_script dla kazdego adresu ip dla aplikacji
                 for app in inter_dict:
                     if app in addr_dict:
                         inter_dict = {app: list(inter_dict[app]) for app in inter_dict}
@@ -81,36 +65,36 @@ def setup(name, intercepted):
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:#otwarcie gniazda UDP
         
         s.connect((HOST, PORT))
-        config_parser_dict, addr_dict, inter_dict = setup('config.ini','ip_intercepted.json')
+        config_parser_dict, addr_dict, inter_dict = setup('config.ini','ip_intercepted.json')#wywolanie funkcji setup
         try:
             while True: 
                 
                 
-                lines = s.recv(1024000)
+                lines = s.recv(1024000) #odebranie na gniezdzie duzej ilosci danych z netifyd
+
                 
                 
                 
                 for line in lines.decode().split('\n'):
                     if line != '':
-                        data = json.loads(line)#TWORZY SLOWNIK Z DANYCH W JSONIE
+                        data = json.loads(line)#tworzenie slownika z danych w jsonie
                         if 'flow' in data:
                             print("interface:\n", data['interface'])
                             print("name:\n", data['flow']['detected_application_name'])
                             
                                 
                             addr = data['flow']['other_ip']
-                                #ip_ver = data['flow']['ip_version']
-                            for application in config_parser_dict:
-                                if application in data['flow']['detected_application_name']:
-                                    if addr not in addr_dict[application]:
-                                        addr_dict[application].append(addr)
-                                        set_rule(config_parser_dict[application],addr)
-                                        #print (addr_list)
+                #operacje majace na celu wyodrebnienie aplikacji i adresu ip
+                            for application in config_parser_dict:#dla kazdej aplikacji w slowniku konfiguracji
+                                if application in data['flow']['detected_application_name']:# jesli obecnie netifyd zidentyfikowalo aplikacje ze slownika
+                                    if addr not in addr_dict[application]:#jesli obecnie przechwycony adres nie pojawil sie dotad dla tej aplikacji
+                                        addr_dict[application].append(addr)#dolacz adres do slownika adresow pod kluczem odpowiedniej aplikacji
+                                        set_rule(config_parser_dict[application],addr)#ustaw zasade zgodnie z parametrami ze slownika konfiguracji
                                         print(json.dumps(data, indent=2))
-                                        #f.write(addr + '\n')
+
                                         
 
                                             
@@ -124,7 +108,7 @@ def main():
 
                 addr_dict = {app: list(addr_dict[app]) for app in addr_dict}
                 json.dump(addr_dict, jsonfile)
-
+ #przechwycone adresy zostaja zapisane w pliku ip_intercepted.json niezaleznie od sposobu w jaki sposob zamknie sie skrypt
             
            
                         
--- applications_tcconfig.py
+++ applications_tcconfig.py
@@ -7,13 +7,13 @@ import logging
 
 from config_parser import load_config, app_dict_ip
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 7150        # The port used by the server
-
-#config_parser_dict = load_config('ex_config.ini')
+HOST = '127.0.0.1'  # Nazwa lub adres IP serwera
+PORT = 7150        # Port dla netifyd
 
 
-def set_rule(options, src_ip):  # przyjmuje liste ustawionych opcji odpowiadajaca aplikacji w slowniku
+
+
+def set_rule(options, src_ip):  # przyjmuje liste ustawionych opcji odpowiadajaca aplikacji w slowniku oraz adres IP wykryty przez netifyd
 
     command = 'tcset '
     command += f'{options["interface"]} '
@@ -27,51 +27,35 @@ def set_rule(options, src_ip):  # przyjmuje liste ustawionych opcji odpowiadajac
     command += f'--add'
     print(command)  # TODO: REMOVE
     proc = subprocess.run(command, stdout=subprocess.PIPE,
-                          stderr=sys.stderr, universal_newlines=True, shell=True)
+                          stderr=sys.stderr, universal_newlines=True, shell=True)# wywolanie stworzonej komendy dla wrappera tcconfig
 
 
-def remove_application_rule(interface, ip_addr, rate):
+def remove_application_rule(interface, ip_addr):
     for addr in ip_addr:
         proc = subprocess.run(f'tcdel {interface} --src-network {addr}', stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE, universal_newlines=True, shell=True)
 
 
-def modify_rule(interface, addr, rate):
-    get_current_rules = subprocess.run(
-        f'tcshow {interface}', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
-    # print(get_current_rules.stdout)
-    current_rules_addresses = []
-    rules_dict = json.loads(get_current_rules.stdout)
-    if interface in rules_dict:
-        for key in rules_dict["br-lan"]["outgoing"]:
-            address = key[key.find('=')+1:key.find('/')]
-            current_rules_addresses.append(address)
-            if addr in current_rules_addresses:
-                modified_rule = subprocess.run(f'tcset {interface} --rate {rate}Kbps --src-network {address} --overwrite',
-                                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
-            else:
-                new_rule = subprocess.run(f'tcset {interface} --rate {rate}Kbps --src-network {addr} --add',
-                                          stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
 
 
-def setup(name, intercepted):
+def setup(name, intercepted):#funkcja setup przyjmuje nazwe pliku konfiguracyjnego i pliku z przechwyconymi adresami
     interfaces = ['eth0', 'br-lan']
-    config_parser_dict = load_config(name)
-    addr_dict = app_dict_ip(config_parser_dict)
-    inter_dict = {}  # miejsce na nowe adresy wg aktualnego pliku config
+    config_parser_dict = load_config(name) #ladowanie konfiguracji z pliku
+    addr_dict = app_dict_ip(config_parser_dict)# stworzenie slownika na zaktualizowane (wszystkie) adresy IP
+    inter_dict = {}  # slownik na adresy IP przechwycone do ip_intercepted
     for i in interfaces:
         proc = subprocess.run(f'tcdel {i} --all ', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                               universal_newlines=True, shell=True)
-    # usunieto poprzednie tc configi
-    if os.path.isfile(intercepted) and os.access(intercepted, os.R_OK):
-        with open(intercepted, 'r') as jsonfile:
-            inter_dict = json.load(jsonfile)
+    # usuniecie wszystkich poprzednich zasad dla interfejsow
+    if os.path.isfile(intercepted) and os.access(intercepted, os.R_OK):#jesli plik intercepted istnieje
+        with open(intercepted, 'r') as jsonfile:#otwarcie pliku
+            inter_dict = json.load(jsonfile)#zaladowanie poprzednich adresow do slownika
             inter_dict = {app: set(inter_dict[app]) for app in inter_dict}
             if inter_dict:  # jesli cos jest w tym slowniku to ustawiam te zasady ponownie
                 for app, ips in inter_dict.items():  # teraz dla kazdej aplikacji musze uwzglednic config obecny
                     # teraz uwzgleniam pojedyncze ip'
                     for i in ips:
-                        set_rule(config_parser_dict[app], i)
+                        set_rule(config_parser_dict[app], i)#wywoluje komende tcconfig dla kazdego adresu ip z intercepted dla aplikacji
                 for app in inter_dict:
                     if app in addr_dict:
                         inter_dict = {
@@ -83,15 +67,15 @@ def setup(name, intercepted):
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:#otwarcie gniazda UDP
 
         s.connect((HOST, PORT))
         config_parser_dict, addr_dict, inter_dict = setup(
-            'config.ini', 'ip_intercepted.json')
+            'config.ini', 'ip_intercepted.json') #wywolanie funkcji setup
         try:
             while True:
 
-                lines = s.recv(1024000)
+                lines = s.recv(1024000) #odebranie na gniezdzie duzej ilosci danych z netifyd
 
                 for line in lines.decode().split('\n'):
                     if line != '':
@@ -101,14 +85,14 @@ def main():
                             print("name:\n", data['flow']
                                   ['detected_application_name'])
 
-                            addr = data['flow']['other_ip']
-
-                            for application in config_parser_dict:
-                                if application in data['flow']['detected_application_name']:
-                                    if addr not in addr_dict[application]:
-                                        addr_dict[application].append(addr)
+                            addr = data['flow']['other_ip'] 
+                #operacje majace na celu wyodrebnienie aplikacji i adresu ip
+                            for application in config_parser_dict: #dla kazdej aplikacji w slowniku konfiguracji
+                                if application in data['flow']['detected_application_name']:# jesli obecnie netifyd zidentyfikowalo aplikacje ze slownika
+                                    if addr not in addr_dict[application]:#jesli obecnie przechwycony adres nie pojawil sie dotad dla tej aplikacji
+                                        addr_dict[application].append(addr)#dolacz adres do slownika adresow pod kluczem odpowiedniej aplikacji
                                         set_rule(
-                                            config_parser_dict[application], addr)
+                                            config_parser_dict[application], addr)#ustaw zasade zgodnie z parametrami ze slownika konfiguracji
 
                                         print(json.dumps(data, indent=2))
 
@@ -121,7 +105,7 @@ def main():
 
                 addr_dict = {app: list(addr_dict[app]) for app in addr_dict}
                 json.dump(addr_dict, jsonfile)
-
+        #przechwycone adresy zostaja zapisane w pliku ip_intercepted.json niezaleznie od sposobu w jaki sposob zamknie sie skrypt
 
 if __name__ == '__main__':
     main()
